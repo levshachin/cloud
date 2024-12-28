828e0079df3bcf90e793f1c8506bf26a925f72ab
levshachin <lewa.shachin@yandex.ru>
2024-12-28 06:37:42 +0300

    icon

--- CloudSell/src/Components/Header.jsx
+++ CloudSell/src/Components/Header.jsx
@@ -1,8 +1,10 @@
+import icon from './icon.png'
 export default function Header(){
     return(
         <header>
-            
+            <img src={icon} alt="" className='icon'/>
             <input type="text" className="falseinput"/>
+            <div></div>
         </header>
     )
 }
